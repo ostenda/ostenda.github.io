959c3decc4bebecb2eac37a57d0264002cbf150b
ostenda <ostenda95@gmail.com>
2023-03-26 17:33:24 +0100

    imports

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import * as THREE from 'three';
+import * as THREEx from './node_modules/@ar-js-org/ar.js/three.js/build/ar-threex-location-only.js';
+
 const canvas = document.getElementById('canvas1');
 
 const scene = new THREE.Scene();
