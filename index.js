f5bf6ac445f0f97764999059ab8cd5a6f2d416ea
ostenda <ostenda95@gmail.com>
2023-03-28 10:01:29 +0100

    gps

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-AFRAME.registerComponent("poi", {
+AFRAME.registerComponent("poi-downloader", {
     init: function() {
-        let update = false;
+        let downloaded = false;
 
         this.el.addEventListener("gps-camera-update-position", async(e) => {
-            if(!update) {
+            if(!downloaded) {
+                const west = e.detail.position.longitude - 0.05,
+                      east = e.detail.position.longitude + 0.05,
+                      south = e.detail.position.latitude - 0.05;
+                      north = e.detail.position.latitude + 0.05;
                 const response = await fetch(`https://hikar.org/webapp/map?bbox=${west},${south},${east},${north}&layers=poi&outProj=4326`);
-                
-                const west = e.detail.position.longitude - 0.01,
-                      east = e.detail.position.longitude + 0.01,
-                      south = e.detail.position.latitude - 0.01;
-                      north = e.detail.position.latitude + 0.01;
-                      const pois = await response.json();
+                const pois = await response.json();
                 pois.features.forEach ( feature => {
                     const compoundEntity = document.createElement("a-entity");
                     compoundEntity.setAttribute('gps-new-entity-place', {
@@ -23,18 +22,28 @@ AFRAME.registerComponent("poi", {
                         y: 20,
                         z: 20
                     });
-                    box.setAttribute('material', { color: 'green' } );
+                    box.setAttribute('material', { color: 'red' } );
                     box.setAttribute("position", { 
                         x : 0,
                         y : 20,
                         z: 0 
                     } );
-                    
+                    const text = document.createElement("a-text");
+                    const textScale = 100;
+                    text.setAttribute("look-at", "[gps-new-camera]");
+                    text.setAttribute("scale", {
+                        x: textScale,
+                        y: textScale,
+                        z: textScale 
+                    });
+                    text.setAttribute("value", feature.properties.name);
+                    text.setAttribute("align", "center");
                     compoundEntity.appendChild(box);
+                    compoundEntity.appendChild(text);
                     document.querySelector("a-scene").appendChild(compoundEntity);
                 });
             }
-            update = true;
+            downloaded = true;
         });
     }    
 });
